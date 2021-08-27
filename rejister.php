3575058ddc34c8a0e27d08f3d2dd80f3e252571b
DAVIDADNAN <89123400+DAVIDADNAN@users.noreply.github.com>
2021-08-28 01:09:37 +0600

    Add files via upload

---
+++ rejister.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+
+<head>
+	<title>signup | New World</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="assets/css/demo.css">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box ">
+					<div class="left">
+						<div class="content">
+							<div class="header">
+								<div class="logo text-center"><img src="assets/img/logo-dark.png" alt="Klorofil Logo"></div>
+								<p class="lead">SIGNUP to your account</p>
+							</div>
+							<form class="form-auth-small" action="index.php">
+								<div class="form-group">
+									<label for="name" class="control-label sr-only">Full name</label>
+									<input type="text" class="form-control" placeholder="Enter Your Name">
+								</div>
+                                <div class="form-group">
+									<label for="signin-email" class="control-label sr-only">Email</label>
+									<input type="email" class="form-control" id="signin-email" placeholder="Email">
+								</div>
+                                <div class="form-group">
+									<label for="mobile" class="control-label sr-only">Mobile Number</label>
+									<input type="mobile" class="form-control" placeholder="Enter your Number">
+								</div>
+                                <div class="form-group">
+									<label for="password" class="control-label sr-only">Password</label>
+									<input type="password" class="form-control" placeholder="Enter Your Password">
+								</div>
+								<div class="form-group">
+									<label for="password" class="control-label sr-only">Confrim Password</label>
+									<input type="password" class="form-control" id="password" placeholder="Password">
+								</div>
+                                <div class="form-group">
+									<label for="University" class="control-label sr-only">University Name</label>
+									<input type="text" class="form-control" id="password" placeholder="Enter Your UNiversity Name">
+								</div>
+								<div class="form-group clearfix">
+									<label class="fancy-checkbox element-left">
+										<input type="checkbox">
+										<span>Remember me</span>
+									</label>
+								</div>
+								<button type="submit" class="btn btn-primary btn-lg btn-block">SIGNUP</button>
+								<div class="bottom">
+									<span class="helper-text"><i class="fa fa-lock"></i> <a href="#">Forgot password?</a></span>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="right">
+						<div class="overlay"></div>
+						<div class="content text">
+							<h1 class="heading">Free Bootstrap dashboard template</h1>
+							<p>by The Develovers</p>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+
+</html>
